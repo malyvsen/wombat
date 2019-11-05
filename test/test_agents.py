0e41e7582c1fa6f11dabc7b87dc319b4930bb29d
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-11-05 09:59:16 +0100

    update dqn agent test

--- test/test_agents.py
+++ test/test_agents.py
@@ -1,5 +1,5 @@
 import gym
-import tensorflow as tf
+import torch
 import numpy as np
 import wombat
 
@@ -8,23 +8,23 @@ env = gym.make('CartPole-v0')
 
 
 def test_dqn():
-    observations = tf.placeholder(tf.float32, shape=(None, env.observation_space.shape[0]))
-    actions = tf.placeholder(tf.int32, shape=(None,))
-    actions_one_hot = tf.one_hot(actions, env.action_space.n)
+    model = torch.nn.Sequential(
+        torch.nn.Linear(env.observation_space.shape[0], 32),
+        torch.nn.ReLU(),
+        torch.nn.Linear(32, env.action_space.n))
 
-    inputs = tf.concat((observations, actions_one_hot), -1)
-    intermediate = tf.layers.Dense(32, activation=tf.nn.relu)(inputs)
-    expected_rewards = tf.layers.Dense(1)(intermediate)
+    optimizer = torch.optim.Adam(model.parameters(), lr=5e-3)
+    def optimize(observation, action, target_reward):
+        prediction = model(torch.tensor(observation, dtype=torch.float32))[action]
+        loss = (prediction - target_reward) ** 2
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        return loss.item()
 
-    true_rewards = tf.placeholder(tf.float32, shape=(None,))
-    loss = tf.losses.mean_squared_error(tf.reshape(true_rewards, (-1, 1)), expected_rewards)
-    optimize = tf.train.AdamOptimizer(2e-3).minimize(loss)
-
-    with tf.Session() as session:
-        session.run(tf.global_variables_initializer())
-        agent = wombat.agents.DQN(env.action_space.n, observations, actions, expected_rewards, true_rewards, loss, optimize, session)
-        training_episodes = wombat.run(agent=agent, environment=env, num_episodes=192, per_step=wombat.train.online(), per_episode=wombat.train.offline())
-        assert np.mean(list(episode.total_reward() for episode in training_episodes[-64:])) > 100
+    agent = wombat.agents.DQN(lambda obs: model(torch.tensor(obs, dtype=torch.float32)).data.numpy(), optimize)
+    episodes = wombat.run(agent=agent, environment=env, num_episodes=256, per_episode=wombat.train.offline())
+    assert np.mean([episode.total_reward() for episode in episodes[-64:]]) > 100
 
 
 def test_random_discrete():
